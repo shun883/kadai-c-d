971cf969f45153d96288c1156fabdd4a243cc07e
hayamishun <shun.883@goole.com>
2019-11-06 12:35:06 +0900

    c-3完成

--- customer.py
+++ customer.py
@@ -7,6 +7,12 @@ class Customer:
     def full_name(self):
         print(self.first_name, self.family_name)
 
+    def entry_fee(self):
+        if self.age < 20:
+            return 1000
+        if self.age < 65:
+            return 1500
+        return 1200
 
 # c-1
 # ken = Customer(first_name='Ken', family_name='Tanaka')
@@ -16,15 +22,27 @@ class Customer:
 # tom.full_name()
 
 # c-2
-ken = Customer(first_name='Ken', family_name='Tanaka', age=15)
-ken_age = ken.age
-print(ken_age)
+# ken = Customer(first_name='Ken', family_name='Tanaka', age=15)
+# ken_age = ken.age
+# print(ken_age)
+#
+# tom = Customer(first_name="Tom", family_name="Ford", age=57)
+# tom_age = tom.age
+# print(tom_age)
+#
+# ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+# ieyasu_age = ieyasu.age
+# print(ieyasu_age)
+
+# c-3
+ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+ken_fee = ken.entry_fee()
+print(ken_fee)
 
 tom = Customer(first_name="Tom", family_name="Ford", age=57)
-tom_age = tom.age
-print(tom_age)
+tom_fee = tom.entry_fee()
+print(tom_fee)
 
 ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
-ieyasu_age = ieyasu.age
-print(ieyasu_age)
-# c-3
+ieyasu_fee = ieyasu.entry_fee()
+print(ieyasu_fee)
